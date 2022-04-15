0c501837f31a136e906e33abde8ee3d1508ccfda
BenjaminBosnjic <bijelivo@gmail.com>
2022-04-15 17:52:46 +0200

    beginnings

--- wcag-compliant-app/src/app/components/policlinic-app/policlinic-app.component.ts
+++ wcag-compliant-app/src/app/components/policlinic-app/policlinic-app.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-policlinic-app',
+  selector: 'policlinic-app',
   templateUrl: './policlinic-app.component.html',
   styleUrls: ['./policlinic-app.component.scss']
 })
