96d309f973673e31240cd61e11e02b88fc97e312
BenjaminBosnjic <bijelivo@gmail.com>
2022-08-03 11:12:33 +0200

    second commit

--- wcag-compliant-app/src/app/app.module.ts
+++ wcag-compliant-app/src/app/app.module.ts
@@ -1,20 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { MatMenuModule } from '@angular/material/menu';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PoliclinicAppComponent } from './components/policlinic-app/policlinic-app.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { NavigationComponent } from './components/navigation/navigation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PoliclinicAppComponent
+    PoliclinicAppComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    NoopAnimationsModule,
+    MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
